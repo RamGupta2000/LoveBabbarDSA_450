04538bfceee6d1d98c4cea00e6e42381b71bee87
RamGupta2000 <ramguptaa501@gmail.com>
2022-04-16 15:47:02 +0530

    LoveBabbarDSA450

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,20 +1,18 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution
 {
 public:
-    bool isSafe(vector<vector<int>> &image, vector<vector<int>> &vis, int x, int y, int n, int m, int src)
+    bool isSafe(vector<vector<int>> &image, vector<vector<int>> &vis, int x, int y, int n, int m,, int src)
     {
-        if (x < 0 || x >= n || y < 0 || y >= m || image[x][y] != src || vis[x][y] == 1)
-            return false;
-        return true;
+        if ((image[x][y] == src) && (vis[x][y] == 0) && (x >= 0 && x < n) && (y >= 0 && y < m))
+            return true;
+        return false;
     }
     void solve(vector<vector<int>> &image, vector<vector<int>> &vis, int x, int y, int newColor, int n, int m, int src)
     {
-        if (x == n && y == m)
-            return;
-
         vis[x][y] = 1;
         image[x][y] = newColor;
 
@@ -22,34 +20,46 @@ public:
         int newx = x - 1;
         int newy = y;
         if (isSafe(image, vis, newx, newy, n, m, src))
-            solve(image, vis, newx, newy, newColor, n, m, src);
+        {
+            // image[newx][newy] = newColor;
+            solve(image, vis, newx, newy, newColor, n, m, int src);
+        }
 
         // down
         newx = x + 1;
         newy = y;
         if (isSafe(image, vis, newx, newy, n, m, src))
-            solve(image, vis, newx, newy, newColor, n, m, src);
+        {
+            // image[newx][newy] = newColor;
+            solve(image, vis, newx, newy, newColor, n, m, int src);
+        }
 
         // left
         newx = x;
         newy = y - 1;
         if (isSafe(image, vis, newx, newy, n, m, src))
-            solve(image, vis, newx, newy, newColor, n, m, src);
+        {
+            // image[newx][newy] = newColor;
+            solve(image, vis, newx, newy, newColor, n, m, int src);
+        }
 
         // right
         newx = x;
         newy = y + 1;
         if (isSafe(image, vis, newx, newy, n, m, src))
-            solve(image, vis, newx, newy, newColor, n, m, src);
+        {
+            // image[newx][newy] = newColor;
+            solve(image, vis, newx, newy, newColor, n, m, int src);
+        }
+        vis[x][y] = 0;
     }
     vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int newColor)
     {
         int n = image.size();
         int m = image[0].size();
-        vector<vector<int>> vis(n + 1, vector<int>(m + 1, 0));
+        vector<vector<int>> vis(n, vector<int>(m, 0));
 
         int src = image[sr][sc];
-        image[sr][sc] = newColor;
         solve(image, vis, sr, sc, newColor, n, m, src);
         return image;
     }
